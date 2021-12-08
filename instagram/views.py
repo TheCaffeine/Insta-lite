58d9572729c05ce5ec23ff2510987e1f80968191
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2021-12-08 11:27:34 +0300

    Validate and save the post form inputs to allow the user see the post

--- instagram/views.py
+++ instagram/views.py
@@ -30,7 +30,15 @@ def signup(request):
 def index(request):
     images = Post.objects.all()
     users = User.objects.exclude(id=request.user.id)
-    
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.profile
+            post.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = PostForm()
     params = {
         'images': images,
         'form': form,
