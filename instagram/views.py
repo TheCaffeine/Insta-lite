8a515c25598a24dabbd5f5258545ba4e1846dc98
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2021-12-08 12:37:07 +0300

    Update views.py

--- instagram/views.py
+++ instagram/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect, 
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
-from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm, PostForm, 
+from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm, PostForm, CommentForm
 from django.contrib.auth import login, authenticate
-from .models import Post
+from .models import Post, Comment, Profile, Follow
 from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from django.views.generic import RedirectView
