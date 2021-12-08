de35ef01cad1ab52787d2cace10595e91ba6436a
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2021-12-08 11:26:34 +0300

    Update views.py

--- instagram/views.py
+++ instagram/views.py
@@ -30,15 +30,7 @@ def signup(request):
 def index(request):
     images = Post.objects.all()
     users = User.objects.exclude(id=request.user.id)
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.user = request.user.profile
-            post.save()
-            return HttpResponseRedirect(request.path_info)
-    else:
-        form = PostForm()
+    
     params = {
         'images': images,
         'form': form,
