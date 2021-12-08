f6cb1bd2d0ebed0a730e4765bbed4ced36b7b0e1
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2021-12-08 11:11:44 +0300

    importinmg the forms here

--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
-
+from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm, PostForm, CommentForm
 from django.contrib.auth import login, authenticate
 from .models import Post, Comment, Profile, Follow
 from django.contrib.auth.models import User
